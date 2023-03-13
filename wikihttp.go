8b5e1a08a19764e18a2574337e03056310cc1bf2
Bandar1 <fakeemail@gmail.com>
2023-03-13 13:31:47 +0300

    templates caching

--- wikihttp.go
+++ wikihttp.go
@@ -14,6 +14,8 @@ type Page struct {
 	Body  []byte
 }
 
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
@@ -29,17 +31,11 @@ func loadPage(title string) (*Page, error) {
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-	t, err := template.ParseFiles(tmpl + ".html")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	err = t.Execute(w, p)
+	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/view/TestPage", http.StatusFound)
 }
